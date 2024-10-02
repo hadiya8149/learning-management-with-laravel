9846e10ef7bb21734c0cb678bb2d5b235492ab00
hadiya8149 <hadiya8149@gmail.com>
2024-10-02 13:50:06 +0500

    Update APIRequestLogs.php

--- app/Http/Middleware/APIRequestLogs.php
+++ app/Http/Middleware/APIRequestLogs.php
@@ -16,51 +16,41 @@ class APIRequestLogs
      */
     public function handle(Request $request, Closure $next)
     {
-        // $startTime = microtime(true);
-        // $controllerAction = optional($request->route())->getActionName();
-        // $middleware = implode(',', array_keys($request->route()->middleware() ?? []));
-        
-        // $apiRequestLog = APIRequestLog::create([
-        //     'method'=>$request->method(),
-        //     'controller_action'=>$controllerAction,
-        //     'middleware'=>$middleware,
-        //     'path'=>$request->path(),
-        //     'ip_address'=>$request->ip(),
-        //     'request_headers'=>json_encode($request->headers->all()),
-        // ]);
-        
-
-        // $request->request_id = $apiRequestLog->id;
-        // $request->start_time = $startTime;
-        return $next($request);
-    }
-    public function terminate(Request $request, $response): void
-    {
-             
         $startTime = microtime(true);
-
-        $duration = microtime(true)-$startTime;
-
-        $status = $response->status();
-        $responseJson = $response->getContent();
-        $memoryUsage = number_format(memory_get_usage()  / 1024 / 1024, 2)." MB";
-
         $controllerAction = optional($request->route())->getActionName();
         $middleware = implode(',', array_keys($request->route()->middleware() ?? []));
+        
         $apiRequestLog = APIRequestLog::create([
             'method'=>$request->method(),
             'controller_action'=>$controllerAction,
             'middleware'=>$middleware,
             'path'=>$request->path(),
-            'status'=>$status,
-            'duration'=>number_format($duration, 4) . ' s',
             'ip_address'=>$request->ip(),
             'request_headers'=>json_encode($request->headers->all()),
+        ]);
+        
+        $request->request_id = $apiRequestLog->id;
+        $request->start_time = $startTime;
+        return $next($request);
+    }
+    public function terminate(Request $request, $response): void
+    {
+        $duration = microtime(true)-$request->start_time;
+
+        $status = $response->status();
+        $responseJson = $response->getContent();
+        $memoryUsage = number_format(memory_get_usage()  / 1024 / 1024, 2)." MB";
+// comment
+        $controllerAction = optional($request->route())->getActionName();
+        $middleware = implode(',', array_keys($request->route()->middleware() ?? []));
+        $apiRequestLog = APIRequestLog::where('id',$request->request_id)->first();
+        $apiRequestLog->update([ 
+            'status'=>$status,
+            'duration'=>number_format($duration, 4) . ' s',
             'response_headers'=>json_encode($response->headers->all()),
             'response_json'=>$responseJson,
             'memory_usage'=>$memoryUsage
         ]);
-        $request->id = $apiRequestLog->id;
     
     }
 }
